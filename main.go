5dcb65aea45ede6a50d9f0bfddd81a949bdd5ffc
Huy Nguyen <huy.nguyen@trustingsocial.com>
2020-12-22 17:16:17 +0700

    feat: using uber/fx for dependency injection

--- main.go
+++ main.go
@@ -5,10 +5,6 @@ import (
 
 	"github.com/snowybell/kokoro/router"
 
-	"github.com/snowybell/kokoro/utils"
-
-	r "github.com/snowybell/kokoro/repo"
-
 	"github.com/gofiber/fiber/v2/middleware/logger"
 
 	"github.com/gofiber/fiber/v2/middleware/requestid"
@@ -23,19 +19,7 @@ func main() {
 	app.Use(cors.New())
 	app.Use(requestid.New(), logger.New())
 
-	// Prepare repository
-	repo, err := r.NewRepoDefault()
-	if err != nil {
-		log.Panicf("can not prepare repo, err=%+v", err)
-	}
-
-	// Init JSONWebToken config
-	jwtConfig, err := utils.NewJWTConfig()
-	if err != nil {
-		log.Panicf("can not prepare jwt, err=%+v", err)
-	}
-
 	// Setup routes and launch app
-	router.SetupRoutes(app, jwtConfig, repo)
+	router.SetupRoutes(app)
 	log.Fatal(app.Listen(":3000"))
 }
--- router/router.go
+++ router/router.go
@@ -1,15 +1,20 @@
 package router
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/snowybell/kokoro/handler"
 	Auth "github.com/snowybell/kokoro/handler/auth"
 	"github.com/snowybell/kokoro/middleware"
 	r "github.com/snowybell/kokoro/repo"
 	"github.com/snowybell/kokoro/utils"
+	"go.uber.org/fx"
+	"golang.org/x/oauth2"
 )
 
-func SetupRoutes(app *fiber.App, jwtConfig *utils.JWTConfig, repo r.Repository) {
+func Register(app *fiber.App, repo r.Repository,
+	jwtConfig *utils.JWTConfig, gOAuthConfig *oauth2.Config) {
 	v1 := app.Group("/v1")
 	v1.Get("/me", middleware.Protected(jwtConfig), handler.Me)
 
@@ -17,4 +22,23 @@ func SetupRoutes(app *fiber.App, jwtConfig *utils.JWTConfig, repo r.Repository) 
 	auth := v1.Group("/auth")
 	auth.Post("/login", Auth.Login(jwtConfig, repo))
 	auth.Post("/register", Auth.Register(repo))
+}
+
+func SetupRoutes(app *fiber.App) {
+	fxApp := fx.New(
+		fx.Provide(
+			// inject app instance
+			func() *fiber.App {
+				return app
+			},
+			r.NewRepoDefault,
+			utils.NewJWTConfig,
+			utils.NewGoogleOAuthConfig,
+		),
+		fx.Invoke(Register),
+	)
+
+	if err := fxApp.Err(); err != nil {
+		log.Fatalf("can not setup routes, err=%s", err.Error())
+	}
 }
