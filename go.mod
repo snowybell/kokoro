f18ac14ae3ec88ce40d9d3430cdbac1c65348620
Huy Nguyen <huy.nguyen@trustingsocial.com>
2020-12-26 16:56:13 +0700

    feat(google_oauth2_callback): receives token and identity user

--- go.mod
+++ go.mod
@@ -18,12 +18,14 @@ require (
 	go.uber.org/fx v1.13.1
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9 // indirect
-	golang.org/x/net v0.0.0-20201216054612-986b41b23924 // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
-	golang.org/x/sys v0.0.0-20201207223542-d4d67f95c62d // indirect
+	golang.org/x/sys v0.0.0-20201223074533-0d417f636930 // indirect
 	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.36.0
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d // indirect
 	gorm.io/driver/postgres v1.0.5
 	gorm.io/gorm v1.20.8
 )
--- handler/oauth/google.go
+++ handler/oauth/google.go
@@ -1,8 +1,16 @@
 package oauth
 
 import (
+	"context"
 	"strconv"
 	"time"
+
+	"google.golang.org/api/option"
+
+	"github.com/snowybell/kokoro/response"
+
+	"github.com/snowybell/kokoro/utils"
+	gOAuth2 "google.golang.org/api/oauth2/v2"
 
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/oauth2"
@@ -16,8 +24,50 @@ func GoogleLoginRedirect(cfg *oauth2.Config) fiber.Handler {
 	}
 }
 
+type LoginCallBackInput struct {
+	State  string `query:"state" validate:"required"`
+	Code   string `query:"code" validate:"required"`
+	Scopes string `query:"scope" validate:"required"`
+}
+
 func GoogleLoginCallback(cfg *oauth2.Config) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
-		return nil
+		var input LoginCallBackInput
+		if err := utils.ShouldBindQuery(ctx, &input); err != nil {
+			return response.
+				Error(ctx).
+				WithMessage("bad request").End()
+		}
+
+		bgCtx := context.Background()
+		token, err := cfg.Exchange(bgCtx, input.Code)
+		if err != nil {
+			return response.
+				Error(ctx).
+				WithMessage(err.Error()).End()
+		}
+
+		gService, err := gOAuth2.NewService(
+			bgCtx,
+			option.WithScopes(cfg.Scopes...),
+			option.WithTokenSource(cfg.TokenSource(bgCtx, token)))
+		if err != nil {
+			return response.
+				Error(ctx).
+				WithCode(fiber.StatusInternalServerError).
+				WithMessage(err.Error()).End()
+		}
+
+		tokenInfo, err := gService.Tokeninfo().Do()
+		if err != nil {
+			return response.
+				Error(ctx).
+				WithCode(fiber.StatusInternalServerError).
+				WithMessage(err.Error()).End()
+		}
+
+		return response.
+			Success(ctx).
+			WithData(fiber.Map{"info": tokenInfo}).End()
 	}
 }
